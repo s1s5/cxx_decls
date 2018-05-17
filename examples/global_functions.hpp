41593aff4e65a42ee1652cd277b723029d0b755e
Shogo Sawai <shogo.sawai@gmail.com>
2018-05-17 11:21:16 +0900

    update example document

--- examples/global_functions.hpp
+++ examples/global_functions.hpp
@@ -13,31 +13,42 @@
 #include <vector>
 
 // ---------- default ----------
-/**
- *  - java <package_name>.Global
- * public static void func()
- * 
- *  - objc @class bb_Global
- * + (void) bb_func;
- */
 void func();
-
-/**
- *  - java <package_name>.Global
- * public static int return_int();
- * 
- *  - objc @class bb_Global
- * + (int) bb_return_int;
- */
 int return_int();
 
 // ---------- overloaded ----------
 
 int32_t add(int32_t, int32_t);
-double add(double, double);
+double add(double d0, double d1);
 
 std::string say_hello();
 std::vector<std::string> get_names();
 
+/**
+ *  - java <package_name>.Global
+ *
+ *  public static void func() { ... }
+ *  public static int return_int() { ... }
+ *  public static int add(int arg0, int arg1) { ... }
+ *  public static double add(double arg0, double arg1) { ... }
+ *  public static String say_hello() { ... }
+ *  public static String [] get_names() { ... }
+ */
+
+/**
+ *  - objc @class bb_Global
+ * 
+ * @class bb_Global;
+ * @interface bb_Global:NSObject {
+ * }
+ * + (void) bb_func;
+ * + (int) bb_return_int;
+ * + (int) bb_add:(int)arg0 arg1:(int)arg1;
+ * + (double) bb_add:(double)d0 d1:(double)d1;
+ * + (NSString *) bb_say_hello;
+ * + (NSArray<NSString*> *) bb_get_names;
+ * 
+ * @end
+ */
 
 #endif  // GLOBAL_FUNCTIONS_HPP_
